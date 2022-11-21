cc6f81839031da0dfe49e6e0284b1772817a1a3b
dmcbreairty <114577896+dmcbreairty@users.noreply.github.com>
2022-11-21 13:55:37 -0500

    Add a new form for search
    add the java for the input, and give us the information for the input

--- vanillaweatherapp/src/app.js
+++ vanillaweatherapp/src/app.js
@@ -48,7 +48,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "1ft66b38222ac020b260o674ba337ff5";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={New York City}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1ft66b38222ac020b260o674ba337ff5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
