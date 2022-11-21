162f66458a35e1c996b1639e99b448b15fcc1633
dmcbreairty <114577896+dmcbreairty@users.noreply.github.com>
2022-11-21 12:56:21 -0500

    added date to my js

--- vanillaweatherapp/src/app.js
+++ vanillaweatherapp/src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -6,6 +29,7 @@ function displayTemperature(response) {
   let pressureElement = document.querySelector("#pressure");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#sky");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -15,6 +39,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = Math.round(
     response.data.condition.description
   );
+  dateElement.innerHTML = formatDate(response.time * 1000);
 }
 
 let apiKey = "1ft66b38222ac020b260o674ba337ff5";
