1ebce2b167fdab4860b406dc0be6b700207e9f02
dmcbreairty <114577896+dmcbreairty@users.noreply.github.com>
2022-11-21 12:41:16 -0500

    add more elements to my js

--- vanillaweatherapp/src/app.js
+++ vanillaweatherapp/src/app.js
@@ -2,11 +2,19 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let pressureElement = document.querySelector("#pressure");
+  let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#sky");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  pressureElement.innerHTML = response.data.temperature.pressure;
+  windElement.innerHTML = response.data.wind.speed;
+  descriptionElement.innerHTML = Math.round(
+    response.data.condition.description
+  );
 }
 
 let apiKey = "1ft66b38222ac020b260o674ba337ff5";
